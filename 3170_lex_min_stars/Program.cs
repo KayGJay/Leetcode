84c828e9414c397c3e97390d45b3e7ee194a8527
kaygjay <kylegjones13@gmail.com>
2025-07-08 16:05:01 -0500

    Ugh

---
+++ 3170_lex_min_stars/Program.cs
@@ -0,0 +1,27 @@
+﻿//NOT FINISHED, hate this
+
+Console.WriteLine(new Solution().ClearStars("de*"));
+
+public class Solution
+{
+    public string ClearStars(string s)
+    {
+        PriorityQueue<char, int> letters = new PriorityQueue<char, int>();
+        char[] result = new char[s.Length];
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] != '*')
+            {
+                letters.Enqueue(s[i], -i);
+                result[i] = s[i];
+            }
+            else
+            {
+                result[i] = ' ';
+                letters.TryDequeue(out char _, out int priority);
+                result[-priority] = ' ';
+            }
+        }
+        return String.Join("", result).Replace(" ", "");
+    }
+}
