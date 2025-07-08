7906d9deb57f0a3d4a945e71d1f2d2f7a4b394ed
kaygjay <kylegjones13@gmail.com>
2025-07-08 13:14:39 -0500

    Added, solved (originally O(n^2))

---
+++ 2016_max_diff_increasing/Program.cs
@@ -0,0 +1,23 @@
+﻿Solution sol = new Solution();
+
+Console.WriteLine(sol.MaximumDifference([7, 1, 5, 4]));
+
+public class Solution
+{
+    public int MaximumDifference(int[] nums)
+    {
+        int min = nums[0], maxDiff = -1;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > min)
+            {
+                maxDiff = Math.Max(maxDiff, nums[i] - min);
+            }
+            else
+            {
+                min = nums[i];
+            }
+        }
+        return maxDiff;
+    }
+}
