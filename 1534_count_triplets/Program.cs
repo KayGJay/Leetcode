f71221c6783d183b8e2cd5fdfbdfd7d8e11b3eb3
kaygjay <kylegjones13@gmail.com>
2025-07-09 11:35:01 -0500

    Added and Solved 7/9/25 daily, started count triplets

---
+++ 1534_count_triplets/Program.cs
@@ -0,0 +1,34 @@
+﻿Solution sol = new Solution();
+
+Console.WriteLine(sol.CountGoodTriplets([3, 0, 1, 1, 9, 7], 7, 2, 3));
+
+public class Solution
+{
+    public int CountGoodTriplets(int[] arr, int a, int b, int c)
+    {
+        int result = 0;
+        int i = 0;
+        while (i < arr.Length - 2)
+        {
+            int j = i + 1;
+            while (j < arr.Length - 1)
+            {
+                int k = j + 1;
+                while (k < arr.Length)
+                {
+                    if (Math.Abs(arr[i] - arr[j]) <= a
+                        && Math.Abs(arr[j] - arr[k]) <= b
+                        && Math.Abs(arr[i] - arr[k]) <= c
+                        )
+                    {
+                        result++;
+                    }
+                    k++;
+                }
+                j++;
+            }
+            i++;
+        }
+        return result;
+    }
+}
---
+++ 3439_daily_max_free_time/Program.cs
@@ -0,0 +1,31 @@
+﻿Solution sol = new Solution();
+
+//Console.WriteLine(sol.MaxFreeTime(99, 1, [7, 21, 25], [13, 25, 78]));
+Console.WriteLine(sol.MaxFreeTime(5, 1, [1, 3], [2, 5]));
+
+public class Solution
+{
+    public int MaxFreeTime(int eventTime, int k, int[] startTime, int[] endTime)
+    {
+        int maxGap = 0, tempGap = 0;
+        for (int i = 0; i <= startTime.Length; i++)
+        {
+            int lastEnd = i == 0 ? 0 : endTime[i - 1];
+            int nextAdd = i != startTime.Length ? startTime[i] - lastEnd : eventTime - endTime[endTime.Length - 1];
+            tempGap += nextAdd;
+            if (i > k)
+            {
+                if (i == k + 1)
+                {
+                    tempGap -= startTime[0];
+                }
+                else
+                {
+                    tempGap -= startTime[i - (k + 1)] - endTime[i - (k + 1) - 1];
+                }
+            }
+            maxGap = Math.Max(maxGap, tempGap);
+        }
+        return maxGap;
+    }
+}
