30c41f3009289d9ba7e1d5d38c50905e934cf3c5
kaygjay <kylegjones13@gmail.com>
2025-07-10 15:13:36 -0500

    Easy, solved

---
+++ 485_max_consec_ones_easy/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+        int ones = 0, maxOnes = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            ones = nums[i] == 1 ? ones + 1 : 0;
+            if (ones > maxOnes)
+            {
+                maxOnes = ones;
+            }
+        }
+        return maxOnes;
+    }
+}
