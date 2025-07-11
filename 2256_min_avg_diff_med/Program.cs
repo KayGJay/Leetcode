a8f0691a83b792bc065bf2f03aa81a2b6713285c
kaygjay <kylegjones13@gmail.com>
2025-07-11 15:48:35 -0500

    Not sure if 2256 finished, 2402 not finished

---
+++ 2256_min_avg_diff_med/Program.cs
@@ -0,0 +1,36 @@
+﻿Solution sol = new Solution();
+
+sol.MinimumAverageDifference([2, 5, 3, 9, 5, 3]);
+
+public class Solution
+{
+    public int MinimumAverageDifference(int[] nums)
+    {
+        long leftSum = 0, rightSum = nums.Sum(), minAvgDiff = -1, avgDiff = 0;
+        int minIdx = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            leftSum += nums[i];
+            rightSum -= nums[i];
+            Console.WriteLine($"i + 1: {i + 1}, n - i - 1: {nums.Length - i - 1}, leftSum: {leftSum}, rightSum: {rightSum}");
+            if (i < nums.Length - 1)
+            {
+                avgDiff = Math.Abs((leftSum / (i + 1)) - (rightSum / (nums.Length - i - 1)));
+            }
+            else
+            {
+                avgDiff = leftSum / (i + 1);
+            }
+            if (avgDiff == 0)
+            {
+                return i;
+            }
+            if (avgDiff < minAvgDiff || minAvgDiff == -1)
+            {
+                minAvgDiff = avgDiff;
+                minIdx = i;
+            }
+        }
+        return minIdx;
+    }
+}
---
+++ 2402_meeting_rooms_daily_hard/Program.cs
@@ -0,0 +1,65 @@
+﻿//Still incorrect, try a foreach on meetings and use the durations
+//Hint, PriorityQueue<(long endTime, int room), (long, int)> minheap by endtime then room number
+
+Solution sol = new Solution();
+Console.WriteLine(sol.MostBooked(3, [[39, 49], [28, 39], [9, 29], [10, 36], [22, 47], [2, 3], [4, 49], [46, 50], [45, 50], [17, 33]]));
+
+public class Solution
+{
+    public int MostBooked(int n, int[][] meetings)
+    {
+        var freeRooms = new PriorityQueue<int, int>();
+        var currMeetings = new PriorityQueue<int, int>();
+        var timesUsed = new Dictionary<int, int>();
+        meetings = meetings.OrderBy(x => x[0]).ToArray();
+        foreach (var meeting in meetings)
+        {
+            Console.WriteLine(meeting[0] + ", " + meeting[1]);
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            freeRooms.Enqueue(i, i);
+            timesUsed[i] = 0;
+        }
+
+        int dequeuedMeetings = 0;
+        int delay = 0, time = 0, idx = 0;
+        while (dequeuedMeetings != meetings.Length)
+        {
+            Console.WriteLine(dequeuedMeetings);
+            currMeetings.TryPeek(out _, out int endTime);
+            while (endTime == time && currMeetings.Count != 0)
+            {
+                int roomNumber = currMeetings.Dequeue();
+                dequeuedMeetings++;
+                freeRooms.Enqueue(roomNumber, roomNumber);
+                currMeetings.TryPeek(out _, out endTime);
+            }
+            if (idx < meetings.Length && freeRooms.Count == 0 && meetings[idx][1] == time)
+            {
+                delay++;
+            }
+            else
+            {
+                while (idx < meetings.Length && meetings[idx][0] + delay == time)
+                {
+                    bool roomFree = freeRooms.TryDequeue(out int roomNumber, out int _);
+                    if (roomFree)
+                    {
+                        currMeetings.Enqueue(roomNumber, meetings[idx][1] + delay);
+                        timesUsed[roomNumber]++;
+                        idx++;
+                    }
+                    else
+                    {
+                        delay++;
+                    }
+                }
+            }
+            time++;
+        }
+        foreach (var pair in timesUsed) { Console.WriteLine(pair.Key +  ": " + pair.Value); }
+        return timesUsed.MaxBy(x => x.Value).Key;
+    }
+}
