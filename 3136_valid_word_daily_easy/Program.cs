4a489e1b7b548f66ba69f669aa63af0c443ab62c
Kyle Jones <kylegjones13@gmail.com>
2025-07-15 08:10:23 -0500

    Added and solved

---
+++ 3136_valid_word_daily_easy/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Solution
+{
+    public bool IsValid(string word)
+    {
+        string vowels = "aeiouAEIOU";
+        bool vowel = false, consonant = false;
+        if (word.Length < 3)
+        {
+            return false;
+        }
+        else if (!word.All(x => char.IsLetterOrDigit(x)))
+        {
+            return false;
+        }
+        else
+        {
+            foreach (char c in word)
+            {
+                if (char.IsLetter(c))
+                {
+                    if (vowels.Contains(c))
+                    {
+                        vowel = true;
+                    }
+                    else
+                    {
+                        consonant = true;
+                    }
+                }
+                if (vowel && consonant)
+                {
+                    break;
+                }
+            }
+        }
+        return vowel && consonant;
+    }
+}
