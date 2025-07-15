3cbbaffc9d1e519bb50fc7b48c9ae45ff97066e0
Kyle Jones <kylegjones13@gmail.com>
2025-07-15 14:19:20 -0500

    Added and solved

---
+++ 3375_min_ops_equal_k_easy/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int MinOperations(int[] nums, int k)
+    {
+        HashSet<int> needOps = new HashSet<int>();
+        foreach (int i in nums)
+        {
+            if (i < k)
+            {
+                return -1;
+            }
+            else if (i > k)
+            {
+                needOps.Add(i);
+            }
+        }
+        return needOps.Count;
+    }
+}
