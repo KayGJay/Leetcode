2d55dcd332e9ed9389724e347935ee942bd9caee
Kyle Jones <kylegjones13@gmail.com>
2025-07-16 11:07:20 -0500

    Unsolved

---
+++ 1007_min_dom_rotate_med/Program.cs
@@ -0,0 +1,10 @@
+﻿//bunked it up the first time, try again later lol
+Console.WriteLine(new Solution().MinDominoRotations([2, 1, 2, 4, 2, 2], [5, 2, 6, 2, 3, 2]));
+
+public class Solution
+{
+    public int MinDominoRotations(int[] tops, int[] bottoms)
+    {
+        
+    }
+}
