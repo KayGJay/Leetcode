3f9a32cea1e5a5931ea70bac2b49c9b62492d87b
Kyle Jones <kylegjones13@gmail.com>
2025-07-16 08:05:40 -0500

    61 incomplete, 3201 complete

---
+++ 3201_daily_max_len_val_subseq_med/Program.cs
@@ -0,0 +1,107 @@
+﻿Console.WriteLine(new Solution().MaximumLength([1, 2, 1, 1, 2, 1, 2]));
+
+public class Solution
+{
+    public int MaximumLength(int[] nums)
+    {
+        int odds = 0, evens = 0, alters = 1;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] % 2 == 1)
+            {
+                odds++;
+            }
+            if (nums[i] % 2 == 0)
+            {
+                evens++;
+            }
+            if (i != 0 && nums[i] % 2 != nums[i - 1] % 2)
+            {
+                alters++;
+            }
+        }
+        return Math.Max(Math.Max(odds, evens), alters);
+    }
+}
+
+//First solution, O(n^2)
+//public class Solution
+//{
+//    public int MaximumLength(int[] nums)
+//    {
+//        bool foundOdd = false, foundEven = false;
+//        int result = 1;
+//        for (int i = 0; i < nums.Length; i++)
+//        {
+//            if (nums[i] % 2 == 1 && !foundOdd)
+//            {
+//                foundOdd = true;
+//                int j = i + 1;
+//                int subSeqLength = 1;
+//                while (j < nums.Length)
+//                {
+//                    if (nums[j] % 2 == 1)
+//                    {
+//                        subSeqLength++;
+//                    }
+//                    j++;
+//                }
+//                if (subSeqLength > result)
+//                {
+//                    result = subSeqLength;
+//                }
+//                j = i + 1;
+//                subSeqLength = 1;
+//                while (j < nums.Length)
+//                {
+//                    if (nums[j - 1] % 2 != nums[j] % 2)
+//                    {
+//                        subSeqLength++;
+//                    }
+//                    j++;
+//                }
+//                if (subSeqLength > result)
+//                {
+//                    result = subSeqLength;
+//                }
+//            }
+//            else if (nums[i] % 2 == 0 && !foundEven)
+//            {
+//                foundEven = true;
+//                int j = i + 1;
+//                int subSeqLength = 1;
+//                while (j < nums.Length)
+//                {
+//                    if (nums[j] % 2 == 0)
+//                    {
+//                        subSeqLength++;
+//                    }
+//                    j++;
+//                }
+//                if (subSeqLength > result)
+//                {
+//                    result = subSeqLength;
+//                }
+//                j = i + 1;
+//                subSeqLength = 1;
+//                while (j < nums.Length)
+//                {
+//                    if (nums[j - 1] % 2 != nums[j] % 2)
+//                    {
+//                        subSeqLength++;
+//                    }
+//                    j++;
+//                }
+//                if (subSeqLength > result)
+//                {
+//                    result = subSeqLength;
+//                }
+//            }
+//            if (foundOdd && foundEven)
+//            {
+//                break;
+//            }
+//        }
+//        return result;
+//    }
+//}
---
+++ 61_rotate_list_med/Program.cs
@@ -0,0 +1,30 @@
+﻿new Solution().RotateRight(new ListNode(0, new ListNode(1, new ListNode(2, new ListNode(3)))), 2);
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) 
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public ListNode RotateRight(ListNode head, int k)
+    {
+        ListNode tempNode = head;
+        ListNode prevNode = head;
+        int numNodes = 1;
+        while (tempNode.next != null)
+        {
+            prevNode = tempNode;
+            tempNode = tempNode.next;
+            numNodes++;
+        }
+        k = numNodes % k;
+        Console.WriteLine(numNodes);
+        return head;
+    }
+}
