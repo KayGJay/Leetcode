2b6481df844fb5c52b58a6980cf4a5a85327edcd
Kyle Jones <kylegjones13@gmail.com>
2025-07-17 16:28:01 -0500

    Basically just copied editorial, still don't entirely understand why updating the matrix this way works

---
+++ 3202_daily_max_valid_subseq_med/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int MaximumLength(int[] nums, int k)
+    {
+        int[][] remainders = new int[k][];
+        for (int i = 0; i < k; i++)
+        {
+            remainders[i] = new int[k];
+        }
+        int result = 0;
+        foreach (int num in nums)
+        {
+            for (int prev = 0; prev < k; prev++)
+            {
+                remainders[prev][num % k] = remainders[num % k][prev] + 1;
+                result = Math.Max(result, remainders[prev][num % k]);
+            }
+        }
+        return result;
+    }
+}
