586fe07e22746518ce8e03d78a95f4c723153774
Kyle Jones <kylegjones13@gmail.com>
2025-07-21 08:28:18 -0500

    Solved

---
+++ 1957_daily_fancy_string_easy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+public class Solution
+{
+    public string MakeFancyString(string s)
+    {
+        StringBuilder result = new StringBuilder("");
+        int reps = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (i == 0 || s[i] == s[i - 1])
+            {
+                reps++;
+            }
+            else
+            {
+                reps = 1;
+            }
+            if (reps < 3)
+            {
+                result.Append(s[i]);
+            }
+        }
+        return result.ToString();
+    }
+}
