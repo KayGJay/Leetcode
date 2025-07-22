65df2be35bfc4655a3381aa32793a480abe0a93d
Kyle Jones <kylegjones13@gmail.com>
2025-07-22 09:38:24 -0500

    Soved daily, not 3sum

---
+++ 1695_daily_max_erasure_val_med/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine(new Solution().MaximumUniqueSubarray([5, 2, 1, 2, 5, 2, 1, 2, 5]));
+
+public class Solution
+{
+    public int MaximumUniqueSubarray(int[] nums)
+    {
+        Dictionary<int, int> numOccurs = new Dictionary<int, int>();
+        foreach (int i in nums) { numOccurs[i] = 0; }
+        int result = 0;
+        int l = 0, r = 0;
+        int tempSum = 0;
+        while (l < nums.Length && r < nums.Length)
+        {
+            while (r < nums.Length && numOccurs[nums[r]] == 0)
+            {
+                tempSum += nums[r];
+                numOccurs[nums[r]]++;
+                r++;
+            }
+            result = Math.Max(result, tempSum);
+            while (r < nums.Length && nums[l] != nums[r])
+            {
+                numOccurs[nums[l]]--;
+                tempSum -= nums[l];
+                l++;
+            }
+            numOccurs[nums[l]]--;
+            tempSum -= nums[l];
+            l++;
+            if (r < nums.Length)
+            {
+                numOccurs[nums[r]] = 0;
+            }
+        }
+        return result;
+    }
+}
---
+++ 16_3sum_closest_med/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine(new Solution().ThreeSumClosest([-4, -1, 1, 2], 1));
+
+public class Solution
+{
+    public int ThreeSumClosest(int[] nums, int target)
+    {
+        Array.Sort(nums);
+        int result = 0;
+        result += (nums[0] + nums[1] + nums[2]);
+        int i = 3;
+        while (result < target && i < nums.Length)
+        {
+            int newResult = result - nums[i - 3] + nums[i];
+            if (newResult < target)
+            {
+                result = newResult;
+            }
+            else
+            {
+                result = Math.Abs(target - result) < Math.Abs(target - newResult) ? result : newResult;
+                break;
+            }
+        }
+        return result;
+    }
+}
