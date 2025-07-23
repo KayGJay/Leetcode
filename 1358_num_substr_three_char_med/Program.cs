2bdbcaf4b5bd7f4b5101fc52e27d7b32ab1608fd
Kyle Jones <kylegjones13@gmail.com>
2025-07-23 14:51:13 -0500

    Solved

---
+++ 1358_num_substr_three_char_med/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine(new Solution().NumberOfSubstrings("abc"));
+
+//Not exactly optimal but optimal is also O(n)
+public class Solution
+{
+    public int NumberOfSubstrings(string s)
+    {
+        Dictionary<char, int> lets = new Dictionary<char, int>() { { 'a', 0 }, { 'b', 0 }, { 'c', 0 } };
+        int left = 0, right = 0, result = 0;
+        while (left < s.Length && right < s.Length)
+        {
+            lets[s[right]]++;
+            while (left < s.Length && lets['a'] > 0 && lets['b'] > 0 && lets['c'] > 0)
+            {
+                result += s.Length - right;
+                lets[s[left]]--;
+                left++;
+            }
+            right++;
+        }
+        return result;
+    }
+}
