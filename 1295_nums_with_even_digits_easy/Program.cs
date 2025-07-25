8e9857ad5074d98ac17b509610131d423b248c8a
Kyle Jones <kylegjones13@gmail.com>
2025-07-25 13:35:44 -0500

    Solved

---
+++ 1295_nums_with_even_digits_easy/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public int FindNumbers(int[] nums)
+    {
+        int result = 0;
+        foreach (int i in nums)
+        {
+            if (i.ToString().Length % 2 == 0)
+            {
+                result++;
+            }
+        }
+        return result;
+    }
+}
