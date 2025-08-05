df27406ab249f16e7d4e121b7957f9654370e7e7
Kyle Jones <kylegjones13@gmail.com>
2025-08-05 16:27:10 -0500

    Solved

---
+++ 135_candy_hard/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine(new Solution().Candy([1, 6, 10, 8, 7, 3, 2]));
+
+public class Solution
+{
+    public int Candy(int[] ratings)
+    {
+        int[] numCandies = new int[ratings.Length];
+        Array.Fill(numCandies, 1);
+        int result = 0;
+        for (int i = 0; i < ratings.Length; i++)
+        {
+            int leftNeigh = i == 0 ? 0 : i - 1;
+            int rightNeigh = i == ratings.Length - 1 ? ratings.Length - 1 : i + 1;
+            if (ratings[i] > ratings[leftNeigh] && numCandies[i] <= numCandies[leftNeigh])
+            {
+                numCandies[i] = numCandies[leftNeigh] + 1;
+            }
+            if (ratings[i] > ratings[rightNeigh] && numCandies[i] <= numCandies[rightNeigh])
+            {
+                numCandies[i] = numCandies[rightNeigh] + 1;
+            }
+        }
+        for (int i = ratings.Length - 1; i >= 0; i--)
+        {
+            int leftNeigh = i == 0 ? 0 : i - 1;
+            int rightNeigh = i == ratings.Length - 1 ? ratings.Length - 1 : i + 1;
+            if (ratings[i] > ratings[rightNeigh] && numCandies[i] <= numCandies[rightNeigh])
+            {
+                numCandies[i] = numCandies[rightNeigh] + 1;
+            }
+            if (ratings[i] > ratings[leftNeigh] && numCandies[i] <= numCandies[leftNeigh])
+            {
+                numCandies[i] = numCandies[leftNeigh] + 1;
+            }
+            result += numCandies[i];
+        }
+        return result;
+    }
+}
