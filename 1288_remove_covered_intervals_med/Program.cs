2fad5cbe514e6286c8c51f2aab390b79ac0aac38
Kyle Jones <kylegjones13@gmail.com>
2025-08-06 11:09:31 -0500

    Not done, misunderstood

---
+++ 1288_remove_covered_intervals_med/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine(new Solution().RemoveCoveredIntervals([[1, 4], [4, 7], [3, 6]]));
+
+public class Solution
+{
+    public int RemoveCoveredIntervals(int[][] intervals)
+    {
+        HashSet<int> exists = new HashSet<int>();
+        int result = 0;
+        intervals = intervals.OrderBy(x => x[0]).ThenBy(x => -x[1]).ToArray();
+        foreach (var pair in intervals)
+        {
+            if (exists.Contains(pair[0]) && exists.Contains(pair[1] - 1))
+            {
+                result++;
+            }
+            else
+            {
+                int idx = pair[0];
+                while (idx < pair[1])
+                {
+                    exists.Add(idx);
+                    idx++;
+                }
+            }
+        }
+        foreach (var elem in exists) Console.WriteLine(elem);
+        Console.WriteLine();
+        return intervals.Length - result;
+    }
+}
