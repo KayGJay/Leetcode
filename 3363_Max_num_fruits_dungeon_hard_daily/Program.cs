946e7119dfcc3e37a1cf629f64d71b65a32c134a
Kyle Jones <kylegjones13@gmail.com>
2025-08-07 15:58:23 -0500

    Not solved, doesn't handle the alternate value being the same when child 2 and 3 are in the same room

---
+++ 3363_Max_num_fruits_dungeon_hard_daily/Program.cs
@@ -0,0 +1,99 @@
+﻿Console.WriteLine(new Solution().MaxCollectedFruits([[1, 2, 3, 4], [5, 6, 8, 7], [9, 10, 11, 12], [13, 14, 15, 16]]));
+
+public class Solution
+{
+    public int MaxCollectedFruits(int[][] fruits)
+    {
+        int result = 0;
+        int len = fruits.Length;
+        int i = 0, j = 0, i2 = len - 1, j2 = 0;
+        for (; i < fruits.Length && j < fruits.Length; i++, j++)
+        {
+            result += fruits[i][j];
+            fruits[i][j] = 0;
+        }
+        i = 0;
+        j = len - 1;
+        while (i < fruits.Length - 1)
+        {
+            if (i == i2 && j == j2 && i != len - 1 && j != len - 1)
+            {
+                if (fruits[i][j + 1] > fruits[i2 - 1][j])
+                {
+                    j++;
+                }
+                else
+                {
+                    i2--;
+                }
+            }
+
+            result += fruits[i][j] + fruits[i2][j2];
+            fruits[i][j] = 0;
+            fruits[i2][j2] = 0;
+
+            //update child 2
+            if (i == len - 2)
+            {
+                i = len - 1;
+                j = len - 1;
+            }
+            else if (j == len - 1)
+            {
+                if (fruits[i + 1][j] >= fruits[i + 1][j - 1])
+                {
+                    i++;
+                }
+                else
+                {
+                    i++;
+                    j--;
+                }
+            }
+            else
+            {
+                if (fruits[i + 1][j + 1] > fruits[i + 1][j])
+                {
+                    i++;
+                    j++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            //update child 3
+            if (j2 == len - 1)
+            {
+                i2 = len - 1;
+                j2 = len - 1;
+            }
+            else if (i2 == len - 1)
+            {
+                if (fruits[i2][j2 + 1] > fruits[i2 - 1][j2 + 1])
+                {
+                    j2++;
+                }
+                else
+                {
+                    i2--;
+                    j2++;
+                }
+            }
+            else
+            {
+                if (fruits[i2 + 1][j2 + 1] > fruits[i2][j2 + 1])
+                {
+                    i2++;
+                    j2++;
+                }
+                else
+                {
+                    j2++;
+                }
+            }
+        }
+        return result;
+    }
+}
--- 3479_fruits_baskets_seg_tree_daily_med/Program.cs
+++ 3479_fruits_baskets_seg_tree_daily_med/Program.cs
@@ -1,24 +1,32 @@
 ﻿int[] arr = [2, 5, 7, 9, 6, 8, 1, 3];
 
-int[] sum = new int[4 * arr.Length];
+int[] max = new int[4 * arr.Length];
 
 buildST(arr, 0, 0, 7);
 
 foreach (int i in arr) Console.Write(i + " ");
 Console.WriteLine();
-foreach (int i in sum) Console.Write(i + " ");
+foreach (int i in max) Console.Write(i + " ");
 
 void buildST(int[] source, int sumIdx, int left, int right)
 {
     if (left == right)
     {
-        sum[sumIdx] = source[left];
+        max[sumIdx] = source[left];
     }
     else
     {
         int mid = (left + right) / 2;
         buildST(source, sumIdx * 2 + 1, left, mid);
         buildST(source, sumIdx * 2 + 2, mid + 1, right);
-        sum[sumIdx] = Math.Max(sum[sumIdx * 2 + 1], sum[sumIdx * 2 + 2]);
+        max[sumIdx] = Math.Max(max[sumIdx * 2 + 1], max[sumIdx * 2 + 2]);
+    }
+}
+
+int Query(int idx, int left, int right, int qLeft, int qRight)
+{
+    if (qLeft > right || qRight < left)
+    {
+        return int.MinValue;
     }
 }
