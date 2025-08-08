0d719a323f5a17ea4bdda67ca18c0a495755886a
Kyle Jones <kylegjones13@gmail.com>
2025-08-08 09:50:32 -0500

    Don't think I finished 2210? Finished 3396

---
+++ 2210_count_hill_valley_easy/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine(new Solution().CountHillValley([6, 5, 10]));
+
+public class Solution
+{
+    public int CountHillValley(int[] nums)
+    {
+        int result = 0, left = 0, right = 1;
+        while (left < nums.Length && right < nums.Length)
+        {
+            while (right != nums.Length - 1 && nums[right] == nums[right + 1])
+            {
+                right++;
+            }
+            if (right + 1 < nums.Length && ((nums[right + 1] > nums[right] && nums[left] > nums[right])
+                || (nums[left] < nums[right] && nums[right + 1] < nums[right])))
+            {
+                result++;
+                left = right;
+                right = left + 1;
+            }
+            else
+            {
+                left++;
+                right = left + 1;
+            }
+        }
+        return result;
+    }
+}
---
+++ 3396_min_ops_distinct_array_easy/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine(new Solution().MinimumOperations([2, 7, 15, 1, 15]));
+
+//took way too long lol
+public class Solution
+{
+    public int MinimumOperations(int[] nums)
+    {
+        HashSet<int> seen = new HashSet<int>();
+        for (int i = nums.Length - 1; i >= 0; i--)
+        {
+            if (!seen.Add(nums[i]))
+            {
+                return i / 3 + 1;
+            }
+        }
+        return 0;
+    }
+}
