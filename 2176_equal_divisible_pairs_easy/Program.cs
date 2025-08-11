1e2e132ec9d95e91434c343ac236e98fd5a8253b
Kyle Jones <kylegjones13@gmail.com>
2025-08-11 09:39:53 -0500

    Solved

---
+++ 2176_equal_divisible_pairs_easy/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine(new Solution().CountPairs([3, 1, 2, 2, 2, 1, 3], 2));
+
+public class Solution
+{
+    public int CountPairs(int[] nums, int k)
+    {
+        int result = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j] && i * j % k == 0)
+                    {
+                        result++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
