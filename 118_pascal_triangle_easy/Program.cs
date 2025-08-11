25e5244d54dcd0267536978403af17a74fd502ec
Kyle Jones <kylegjones13@gmail.com>
2025-08-11 11:51:35 -0500

    Solved

---
+++ 118_pascal_triangle_easy/Program.cs
@@ -0,0 +1,31 @@
+﻿foreach (var arr in new Solution().Generate(5)) 
+{ 
+    foreach (int i in arr) 
+        Console.Write(i + ", "); 
+    Console.WriteLine();
+}
+
+public class Solution
+{
+
+    private IList<IList<int>> triangle = [];
+
+    public IList<IList<int>> Generate(int numRows)
+    {
+        var triangle = new List<IList<int>>();
+        triangle.Add([1]);
+        for (int i = 1; i < numRows; i++)
+        {
+            var prevRow = triangle[i - 1];
+            var nextRow = new List<int>();
+            nextRow.Add(1);
+            for (int j = 1; j < i; j++)
+            {
+                nextRow.Add(prevRow[j] + prevRow[j - 1]);
+            }
+            nextRow.Add(1);
+            triangle.Add(nextRow);
+        }
+        return triangle;
+    }
+}
