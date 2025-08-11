3eb8005398f7b4b58fbac3a9e165a9e421c070cc
Kyle Jones <kylegjones13@gmail.com>
2025-08-11 11:09:58 -0500

    Solved

---
+++ 1061_lexic_small_equiv_string_med/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine(new Solution().SmallestEquivalentString("leetcode", "programs", "sourcecode"));
+
+public class Solution
+{
+    public string SmallestEquivalentString(string s1, string s2, string baseStr)
+    {
+        Dictionary<char, char> parent = new Dictionary<char, char>();
+        for (int i = 0; i < s1.Length; i++)
+        {
+            char parChar = s1[i] < s2[i] ? s1[i] : s2[i];
+            if (parent.ContainsKey(parChar))
+            {
+                if (parent.ContainsKey(s1[i]))
+                {
+                    parent[parent[s1[i]]] = parent[parChar];
+                }
+                if (parent.ContainsKey(s2[i]))
+                {
+                    parent[parent[s2[i]]] = parent[parChar];
+                }
+                parent[s1[i]] = parent[parChar];
+                parent[s2[i]] = parent[parChar];
+            }
+            else
+            {
+                parent[parChar] = parChar;
+                if (parent.ContainsKey(s1[i]))
+                {
+                    parent[parent[s1[i]]] = parent[parChar];
+                }
+                if (parent.ContainsKey(s2[i]))
+                {
+                    parent[parent[s2[i]]] = parent[parChar];
+                }
+                parent[s1[i]] = parChar;
+                parent[s2[i]] = parChar;
+            }
+        }
+        foreach (var pair in parent) Console.WriteLine(pair.Key + ": " + pair.Value);
+        string result = "";
+        foreach (char ch in baseStr)
+        {
+            result += parent.ContainsKey(ch) ? parent[ch] : ch;
+        }
+        return result;
+    }
+}
---
+++ 3442_max_diff_even_odd_freq_easy/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public int MaxDifference(string s)
+    {
+        int maxOdd = 0, minOdd = Int32.MaxValue, maxEven = 0, minEven = Int32.MaxValue;
+        Dictionary<char, int> occurs = new Dictionary<char, int>();
+        foreach (char c in s)
+        {
+            if (!occurs.ContainsKey(c))
+            {
+                occurs[c] = 0;
+            }
+            occurs[c]++;
+        }
+        foreach (var pair in occurs)
+        {
+            if (pair.Value % 2 == 1)
+            {
+                maxOdd = Math.Max(maxOdd, pair.Value);
+            }
+            else
+            {
+                minEven = Math.Min(minEven, pair.Value);
+            }
+        }
+        return maxOdd - minEven;
+    }
+}
