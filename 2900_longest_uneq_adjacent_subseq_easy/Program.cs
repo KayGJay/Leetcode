99eec270a3043799cbc148f5dbd11e7f18a8ea46
Kyle Jones <kylegjones13@gmail.com>
2025-08-12 14:13:50 -0500

    Solved

---
+++ 2900_longest_uneq_adjacent_subseq_easy/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public IList<string> GetLongestSubsequence(string[] words, int[] groups)
+    {
+        List<string> result = new List<string>();
+        result.Add(words[0]);
+        int lastNum = groups[0];
+        for (int i = 1; i < groups.Length; i++)
+        {
+            if (groups[i] != lastNum)
+            {
+                lastNum = groups[i];
+                result.Add(words[i]);
+            }
+        }
+        return result;
+    }
+}
