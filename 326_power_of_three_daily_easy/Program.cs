ab0b53311264e05a89bbc6f609fadd87d8bfebb7
Kyle Jones <kylegjones13@gmail.com>
2025-08-13 08:22:03 -0500

    Solved

---
+++ 326_power_of_three_daily_easy/Program.cs
@@ -0,0 +1,21 @@
+﻿// Alternate solution, return 1162261467 % n, largest power of 3 that fits in 32 bit int
+
+public class Solution
+{
+    public bool IsPowerOfThree(int n)
+    {
+        if (n % 3 != 0)
+        {
+            return n == 1;
+        }
+        while (n > 1)
+        {
+            if (n % 3 != 0)
+            {
+                return false;
+            }
+            n /= 3;
+        }
+        return n == 1;
+    }
+}
