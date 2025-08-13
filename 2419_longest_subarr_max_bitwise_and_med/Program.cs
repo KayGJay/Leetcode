0082d39769316512a752d7f464f47d7af5c63698
Kyle Jones <kylegjones13@gmail.com>
2025-08-13 10:22:16 -0500

    Suboptimal, better to find max while passing through array for single pass, still O(N)

---
+++ 2419_longest_subarr_max_bitwise_and_med/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine(new Solution().LongestSubarray([3, 3, 3]));
+
+public class Solution
+{
+    public int LongestSubarray(int[] nums)
+    {
+        int max = nums.Max();
+        int result = 1;
+        int tempLen = 1;
+        for (int i = 1; i < nums.Length; i++)
+        { 
+            if (nums[i] == max && nums[i - 1] == max)
+            {
+                tempLen++;
+            }
+            else
+            {
+                result = Math.Max(result, tempLen);
+                tempLen = 1;
+            }
+        }
+        result = Math.Max(result, tempLen);
+        return result;
+    }
+}
