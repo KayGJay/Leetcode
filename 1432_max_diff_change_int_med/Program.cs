d697c70371909aed44ab9a245ed84cbd9083431c
Kyle Jones <kylegjones13@gmail.com>
2025-08-14 13:17:41 -0500

    Solved 2264

---
+++ 1432_max_diff_change_int_med/Program.cs
@@ -0,0 +1,28 @@
+﻿//Not done yet, have to handle most sig dig being 9
+
+Console.WriteLine(new Solution().MaxDiff(123456));
+
+public class Solution
+{
+    public int MaxDiff(int num)
+    {
+        string numStr = num.ToString();
+        char num1 = numStr[0];
+        char num2;
+        int i = 1;
+        if (numStr[0] == '1')
+        {
+            while (numStr[i] == '1')
+            {
+                i++;
+            }
+            num2 = numStr[i];
+        }
+        else
+            num2 = num1;
+        Console.WriteLine(num1 + " " + num2);
+        int max = int.Parse(numStr.Replace(num1, '9'));
+        int min = num2 == num1 ? int.Parse(numStr.Replace(num2, '1')) : int.Parse(numStr.Replace(num2, '0'));
+        return max - min;
+    }
+}
---
+++ 2264_daily_largest_3_same_dig_str/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine(new Solution().LargestGoodInteger("6777133339"));
+
+public class Solution
+{
+    public string LargestGoodInteger(string num)
+    {
+        string result = "";
+        for (int i = 2; i < num.Length; i++)
+        {
+            if (num[i] == num[i - 1] && num[i] == num[i - 2])
+            {
+                if (result == "" || num[i] > result[0])
+                    result = $"{num[i]}{num[i]}{num[i]}";
+            }
+        }
+        return result;
+    }
+}
