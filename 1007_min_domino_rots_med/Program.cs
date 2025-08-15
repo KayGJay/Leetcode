6ca63c4d60be33b534a61bf25adaa864aa29686d
Kyle Jones <kylegjones13@gmail.com>
2025-08-15 11:05:26 -0500

    Started

---
+++ 1007_min_domino_rots_med/Program.cs
@@ -0,0 +1,34 @@
+﻿//Started and going ok but I'm braindead atm
+
+public class Solution
+{
+    public int MinDominoRotations(int[] tops, int[] bottoms)
+    {
+        int minSwaps = Int32.MaxValue;
+        for (int i = 0; i <= 6; i++)
+        {
+            bool poss = true;
+            int currSwaps = 0;
+            for (int j = 0; j < tops.Length; j++)
+            {
+                if (tops[j] != i)
+                {
+                    if (bottoms[j] == i)
+                    {
+                        currSwaps++;
+                    }
+                    else
+                    {
+                        poss = false;
+                        break;
+                    }
+                }
+                if (poss)
+                {
+                    minSwaps = Math.Min(minSwaps, currSwaps);
+                }
+            }
+        }
+        return minSwaps == Int32.MaxValue ? -1 : minSwaps;
+    }
+}
