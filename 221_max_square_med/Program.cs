6c9ac3338a1db9297daa30a38a58ceef50b02093
Kyle Jones <kylegjones13@gmail.com>
2025-08-20 09:27:01 -0500

    Solved

---
+++ 221_max_square_med/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine(new Solution().MaximalSquare([['1','0','1','0','0'], ['1','0','1','1','1'], ['1','1','1','1','1'], ['1','0','0','1','0']]));
+
+public class Solution
+{
+    public int MaximalSquare(char[][] matrix)
+    {
+        int[][] intMatrix = new int[matrix.Length][];
+        int result = 0;
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            intMatrix[i] = new int[matrix[i].Length];
+            for (int j = 0; j < matrix[i].Length; j++)
+            {
+                if (matrix[i][j] == '1')
+                {
+                    if (i == 0 || j == 0)
+                    {
+                        intMatrix[i][j] = 1;
+                        result = Math.Max(1, result);
+                    }
+                    else
+                    {
+                        intMatrix[i][j] = Math.Min(Math.Min(intMatrix[i - 1][j - 1], intMatrix[i][j - 1]), intMatrix[i - 1][j]) + 1;
+                        result = Math.Max(intMatrix[i][j] * intMatrix[i][j], result);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ 3169_count_days_wo_meetings_med/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(new Solution().CountDays(57, [[3, 49], [23, 44], [21, 56], [26, 55], [23, 52], [2, 9], [1, 48], [3, 31]]));
+public class Solution
+{
+    public int CountDays(int days, int[][] meetings)
+    {
+        int result = 0;
+        meetings = meetings.OrderBy(x => x[0]).ToArray();
+        foreach (var arr in meetings) { Console.WriteLine(arr[0] + ", " + arr[1]); }
+        result += meetings[0][0] - 1;
+        int prevStart = meetings[0][0];
+        int latest = meetings[0][1];
+        foreach (var meeting in meetings)
+        {
+            if (meeting[0] != prevStart)
+            {
+                result += latest < meeting[0] ? meeting[0] - (latest + 1) : 0;
+                latest = Math.Max(latest, meeting[1]);
+                prevStart = meeting[0];
+            }
+            else
+            {
+                latest = Math.Max(latest, meeting[1]);
+            }
+        }
+        result += days - latest;
+        return result;
+    }
+}
