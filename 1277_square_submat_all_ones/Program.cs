282164370eeecf2e81b74d605c052619e64c9b1a
Kyle Jones <kylegjones13@gmail.com>
2025-08-20 09:44:18 -0500

    Solved

---
+++ 1277_square_submat_all_ones/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine(new Solution().CountSquares([[1, 0, 1], [1, 1, 0], [1, 1, 0]]));
+
+public class Solution
+{
+    public int CountSquares(int[][] matrix)
+    {
+        int result = 0;
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            for (int j = 0; j < matrix[i].Length; j++)
+            {
+                if (i == 0 || j == 0)
+                {
+                    result += matrix[i][j];
+                }
+                else
+                {
+                    if (matrix[i][j] == 1)
+                    { 
+                        matrix[i][j] = Math.Min(Math.Min(matrix[i][j - 1], matrix[i - 1][j]), matrix[i - 1][j - 1]) + 1;
+                        result += matrix[i][j];
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
