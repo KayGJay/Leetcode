47a0aa21309a025288af5b5d01f673c98ee5e680
Kyle Jones <kylegjones13@gmail.com>
2025-08-25 10:34:36 -0500

    Solved

---
+++ 3195_daily_min_area_cover_1s_med/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public int MinimumArea(int[][] grid)
+    {
+        int recIMin = Int32.MaxValue, recJMin = Int32.MaxValue;
+        int recIMax = 0, recJMax = 0;
+        for (int i = 0; i < grid.Length; i++)
+        {
+            for (int j = 0; j < grid[i].Length; j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    recIMin = Math.Min(recIMin, i);
+                    recJMin = Math.Min(recJMin, j);
+                    recIMax = Math.Max(recIMax, i);
+                    recJMax = Math.Max(recJMax, j);
+                }
+            }
+        }
+        return (recIMax - recIMin + 1) * (recJMax - recJMin + 1);
+    }
+}
---
+++ 498_diag_traverse_daily_med/Program.cs
@@ -0,0 +1,49 @@
+﻿foreach (int i in new Solution().FindDiagonalOrder([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])) Console.WriteLine(i);
+
+public class Solution
+{
+    public int[] FindDiagonalOrder(int[][] mat)
+    {
+        Dictionary<int, List<int>> idxSums = new Dictionary<int, List<int>>();
+        int[] result = new int[mat.Length * mat[0].Length];
+        for (int i = 0; i < mat.Length; i++)
+        {
+            for (int j = 0; j < mat[i].Length; j++)
+            {
+                if (!idxSums.ContainsKey(i + j))
+                {
+                    idxSums[i + j] = new List<int>();
+                }
+                idxSums[i + j].Add(mat[i][j]);
+            }
+        }
+        foreach (var pair in idxSums) 
+        {
+            Console.Write(pair.Key + ": ");
+            foreach (int i in pair.Value)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+        int resultIdx = 0;
+        foreach (int i in idxSums.Keys)
+        {
+            if (i % 2 == 0)
+            {
+                for (int j = idxSums[i].Count - 1; j >= 0; j--)
+                {
+                    result[resultIdx++] = idxSums[i][j];
+                }
+            }
+            else
+            {
+                for (int j = 0; j < idxSums[i].Count; j++)
+                {
+                    result[resultIdx++] = idxSums[i][j];
+                }
+            }
+        }
+        return result;
+    }
+}
