9916c65ac5c222a888c18a5efac44a9e3184c3a9
Kyle Jones <kylegjones13@gmail.com>
2025-08-26 16:35:54 -0500

    Solved

--- 498_diag_traverse_daily_med/Program.cs
+++ 498_diag_traverse_daily_med/Program.cs
@@ -1,49 +1,66 @@
-﻿foreach (int i in new Solution().FindDiagonalOrder([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])) Console.WriteLine(i);
+﻿Console.WriteLine(new Solution().MyPow(34.00515, -3));
 
 public class Solution
 {
-    public int[] FindDiagonalOrder(int[][] mat)
+    public double MyPow(double x, int n)
     {
-        Dictionary<int, List<int>> idxSums = new Dictionary<int, List<int>>();
-        int[] result = new int[mat.Length * mat[0].Length];
-        for (int i = 0; i < mat.Length; i++)
-        {
-            for (int j = 0; j < mat[i].Length; j++)
-            {
-                if (!idxSums.ContainsKey(i + j))
-                {
-                    idxSums[i + j] = new List<int>();
-                }
-                idxSums[i + j].Add(mat[i][j]);
-            }
-        }
-        foreach (var pair in idxSums) 
-        {
-            Console.Write(pair.Key + ": ");
-            foreach (int i in pair.Value)
-            {
-                Console.Write(i + " ");
-            }
-            Console.WriteLine();
-        }
-        int resultIdx = 0;
-        foreach (int i in idxSums.Keys)
-        {
-            if (i % 2 == 0)
-            {
-                for (int j = idxSums[i].Count - 1; j >= 0; j--)
-                {
-                    result[resultIdx++] = idxSums[i][j];
-                }
-            }
-            else
-            {
-                for (int j = 0; j < idxSums[i].Count; j++)
-                {
-                    result[resultIdx++] = idxSums[i][j];
-                }
-            }
-        }
-        return result;
+        if (x == 1)
+            return 1;
+        if (n == 0)
+            return 1;
+        if (n % 2 == 0)
+            return n > 0 ? MyPow(x * x, n / 2) : 1.0 / MyPow(x * x, -n / 2);
+        else
+            return n > 0 ? x * MyPow(x, n - 1) : 1.0 / (x * MyPow(x * x, -n - 1));
     }
 }
+
+//foreach (int i in new Solution().FindDiagonalOrder([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])) Console.WriteLine(i);
+
+//public class Solution
+//{
+//    public int[] FindDiagonalOrder(int[][] mat)
+//    {
+//        Dictionary<int, List<int>> idxSums = new Dictionary<int, List<int>>();
+//        int[] result = new int[mat.Length * mat[0].Length];
+//        for (int i = 0; i < mat.Length; i++)
+//        {
+//            for (int j = 0; j < mat[i].Length; j++)
+//            {
+//                if (!idxSums.ContainsKey(i + j))
+//                {
+//                    idxSums[i + j] = new List<int>();
+//                }
+//                idxSums[i + j].Add(mat[i][j]);
+//            }
+//        }
+//        foreach (var pair in idxSums) 
+//        {
+//            Console.Write(pair.Key + ": ");
+//            foreach (int i in pair.Value)
+//            {
+//                Console.Write(i + " ");
+//            }
+//            Console.WriteLine();
+//        }
+//        int resultIdx = 0;
+//        foreach (int i in idxSums.Keys)
+//        {
+//            if (i % 2 == 0)
+//            {
+//                for (int j = idxSums[i].Count - 1; j >= 0; j--)
+//                {
+//                    result[resultIdx++] = idxSums[i][j];
+//                }
+//            }
+//            else
+//            {
+//                for (int j = 0; j < idxSums[i].Count; j++)
+//                {
+//                    result[resultIdx++] = idxSums[i][j];
+//                }
+//            }
+//        }
+//        return result;
+//    }
+//}
---
+++ 532_k_diff_paris_med/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Solution
+{
+    public int FindPairs(int[] nums, int k)
+    {
+        int result = 0;
+        Dictionary<int, int> counts = new Dictionary<int, int>();
+        foreach (int i in nums)
+        {
+            if (!counts.ContainsKey(i))
+                counts[i] = 0;
+            counts[i]++;
+        }
+        foreach (int key in counts.Keys)
+        {
+            if (k == 0)
+            {
+                if (counts[key] > 1)
+                {
+                    result++;
+                }
+            }
+            else
+            {
+                if (counts.ContainsKey(key + k))
+                {
+                    if (counts[key + k] != 0)
+                    {
+                        result++;
+                    }
+                }
+                if (counts.ContainsKey(key - k))
+                {
+                    if (counts[key - k] != 0)
+                    {
+                        result++;
+                    }
+                }
+                counts[key] = 0;
+            }
+        }
+        return result;
+    }
+}
