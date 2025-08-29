a12610aae40680fe50df46c4763d7e57831e73e4
Kyle Jones <kylegjones13@gmail.com>
2025-08-29 08:52:15 -0500

    Solved

---
+++ 3446_sort_mat_by_diag_daily_med/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Solution
+{
+    public int[][] SortMatrix(int[][] grid)
+    {
+        int[][] result = new int[grid.Length][];
+        int iOuter = grid.Length - 1;
+        int jOuter = 0;
+        while (iOuter >= 0 && jOuter < grid[0].Length)
+        {
+            List<int> tempList = new List<int>();
+            int i = iOuter, j = jOuter;
+            while (i >= 0 && i < grid.Length && j >= 0 && j < grid[i].Length)
+            {
+                tempList.Add(grid[i][j]);
+                i++;
+                j++;
+            }
+            if (iOuter >= 0 && jOuter == 0)
+                tempList = tempList.OrderByDescending(x => x).ToList();
+            else
+                tempList = tempList.OrderBy(x => x).ToList();
+            i = iOuter;
+            j = jOuter;
+            int tempIdx = 0;
+            while (i >= 0 && i < grid.Length && j >= 0 && j < grid[i].Length)
+            {
+                grid[i][j] = tempList[tempIdx];
+                tempIdx++;
+                i++;
+                j++;
+            }
+            if (iOuter > 0)
+                iOuter--;
+            else
+                jOuter++;
+        }
+        return grid;
+    }
+}
