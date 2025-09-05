263455ed3055a22a90edf64c9f334ed060e5f863
Kyle Jones <kylegjones13@gmail.com>
2025-09-05 11:17:26 -0500

    Unsolved

---
+++ 2749_min_ops_make_int_zero_daily_med/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine(new Solution().MakeTheIntegerZero(3, -2));
+
+//Notsolved, something to do with BitCount()?
+public class Solution
+{
+    public int MakeTheIntegerZero(int num1, int num2)
+    {
+        int zeroToNum = 0;
+        int result = 0;
+        while (zeroToNum < num1)
+        {
+            int i = 1;
+            while (zeroToNum + i + num2 < num1)
+            {
+                Console.WriteLine($"i: {i}");
+                i <<= 1;
+            }
+            zeroToNum += (i >> 1) + num2;
+            result++;
+        }
+        Console.WriteLine(zeroToNum);
+        return zeroToNum == num1 ? result : -1; 
+    }
+}
