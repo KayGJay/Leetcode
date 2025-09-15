35655f8a49a2fdefb56406aa06f84d2b59ac4959
Kyle Jones <kylegjones13@gmail.com>
2025-09-15 10:08:39 -0500

    Solved

---
+++ 2181_merge_between_zeroes_med/Program.cs
@@ -0,0 +1,38 @@
+﻿public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public ListNode MergeNodes(ListNode head)
+    {
+        ListNode currNode = head;
+        ListNode currNew = head;
+        while (currNode != null)
+        {
+            if (currNode.val == 0 && currNode != head)
+            {
+                if (currNode.next != null)
+                {
+                    currNew.next = currNode;
+                    currNew = currNode;
+                }
+                else
+                {
+                    currNew.next = null;
+                }
+            }
+            else
+            {
+                currNew.val += currNode.val;
+            }
+            currNode = currNode.next;
+        }
+        return head;
+    }
+}
