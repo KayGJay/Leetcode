713e94340901cad4e9c65bf7f5bf7ea8ef39f114
Kyle Jones <kylegjones13@gmail.com>
2025-09-15 10:06:47 -0500

    Not sure about 3233, 966 not quite solved, see site

---
+++ 3233_count_nums_not_special_med/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine(new Solution().NonSpecialCount(5, 7));
+public class Solution
+{
+    public int NonSpecialCount(int l, int r)
+    {
+        int result = 0;
+        HashSet<int> sieve = Sieve(r);
+        for (int i = l; i <= r; i++)
+        {
+            double sqrt = Math.Sqrt(i);
+            if (sqrt != (int)sqrt || !sieve.Contains((int)sqrt))
+                result++;
+        }
+        return result;
+    }
+    public HashSet<int> Sieve(int num)
+    {
+        Dictionary<int, bool> primes = new Dictionary<int, bool>();
+        primes[2] = true;
+        for (int i = 3; i < num; i++)
+        {
+            primes[i] = true;
+        }
+        int start = 2;
+        int curNum = start;
+        while (start <= num / 2)
+        {
+            while (curNum <= num)
+            {
+                curNum += start;
+                primes[curNum] = false;
+            }
+            start++;
+            curNum = start;
+        }
+        int[] result = primes.Where(x => x.Value == true).Select(x => x.Key).ToArray();
+        return result.ToHashSet<int>();
+    }
+}
---
+++ 966_vowel_spellcheck_med/Program.cs
@@ -0,0 +1,68 @@
+﻿foreach (var s in new Solution().Spellchecker(["KiTe", "kite", "hare", "Hare"], ["kite", "Kite", "KiTe", "Hare", "HARE", "Hear", "hear", "keti", "keet", "keto"])) Console.WriteLine(s);
+
+public class Solution
+{
+    public bool MatchExceptVowels(string key, string word)
+    {
+        if (key.Length != word.Length) return false;
+        string vowels = "AEIOUaeiou";
+        for (int i = 0; i < word.Length; i++)
+        {
+            if (!vowels.Contains(word[i]) && !vowels.Contains(key[i]))
+            {
+                if (word[i].ToString().ToLower() != key[i].ToString().ToLower())
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public string[] Spellchecker(string[] wordlist, string[] queries)
+    {
+        HashSet<string> words = new HashSet<string>(wordlist);
+        List<string> result = new List<string>();
+        foreach (string s in queries)
+        {
+            bool added = false;
+            foreach (var key in words)
+            {
+                if (s == key)
+                {
+                    result.Add(key);
+                    added = true;
+                    break;
+                }
+            }
+            if (!added)
+            {
+                foreach (var key in words)
+                {
+                    if (s.ToLower() == key)
+                    {
+                        result.Add(key);
+                        added = true;
+                        break;
+                    }
+                }
+            }
+            if (!added)
+            {
+                foreach (var key in words)
+                {
+                    if (MatchExceptVowels(key, s))
+                    {
+                        result.Add(key);
+                        added = true;
+                        break;
+                    }
+                }
+            }
+            if (!added)
+            {
+                result.Add("");
+            }
+        }
+        return result.ToArray();
+    }
+}
