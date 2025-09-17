b53463c3ca222668c277e06d8d236342d52aeb25
Kyle Jones <kylegjones13@gmail.com>
2025-09-17 10:39:21 -0500

    Solved 2353

---
+++ 1663_smallest_string_given_val_med/Program.cs
@@ -0,0 +1,93 @@
+﻿Console.WriteLine(new Solution().GetSmallestString(5, 73));
+
+Console.WriteLine((int)'f' - (int)'a');
+
+public class Solution
+{
+    public Dictionary<int, char> charVals = new Dictionary<int, char>
+    {
+        { 1, 'a' },
+        { 2, 'b' },
+        { 3, 'c' },
+        { 4, 'd' },
+        { 5, 'e' },
+        { 6, 'f' },
+        { 7, 'g' },
+        { 8, 'h' },
+        { 9, 'i' },
+        { 10, 'j' },
+        { 11, 'k' },
+        { 12, 'l' },
+        { 13, 'm' },
+        { 14, 'n' },
+        { 15, 'o' },
+        { 16, 'p' },
+        { 17, 'q' },
+        { 18, 'r' },
+        { 19, 's' },
+        { 20, 't' },
+        { 21, 'u' },
+        { 22, 'v' },
+        { 23, 'w' },
+        { 24, 'x' },
+        { 25, 'y' },
+        { 26, 'z' }
+    };
+    //public string GetSmallestString(int n, int k)
+    //{
+    //    string result = "";
+    //    int value = 0;
+    //    char toAdd = 'a';
+    //    int valToAdd = 0;
+    //    while (result.Length < n)
+    //    {
+    //        Console.WriteLine((k - (value + 1)) + ": " + ((n - (result.Length + 1)) * 26));
+    //        if (k - (value + 1) <= (n - (result.Length + 1)) * 26)
+    //        {
+    //            toAdd = 'a';
+    //            valToAdd = 1;
+    //        }
+    //        else if (k - value <= 26)
+    //        {
+    //            toAdd = charVals[k - value];
+    //            valToAdd = k - value;
+    //        }
+    //        else if ((k - value) % 26 == 0)
+    //        {
+    //            toAdd = 'z';
+    //            valToAdd = 26;
+    //        }
+    //        else
+    //        {
+    //            valToAdd = 2;
+    //            while ((k - value - valToAdd) % 26 != 0)
+    //            {
+    //                valToAdd++;
+    //            }
+    //            toAdd = charVals[valToAdd];
+    //        }
+    //        result += toAdd;
+    //        value += valToAdd;
+    //    }
+    //    return result;
+    //}
+    public string GetSmallestString(int n, int k)
+    {
+        string result = "";
+        if (k - n < 26)
+        {
+            result += new string('a', n - 1);
+            result += charVals[k - result.Length];
+        }
+        else
+        {
+            int value = 0;
+            result += new string('a', k / 26);
+            value += result.Length;
+            result += charVals[(k - value) % 26];
+            value += (k - value) % 26;
+            result += new string('z', (k - value) / 26);
+        }
+        return result;
+    }
+}
---
+++ 2353_daily_food_rating_system_med/Program.cs
@@ -0,0 +1,37 @@
+﻿public class FoodRatings
+{
+    private Dictionary<string, (string, int)> Foods = new Dictionary<string, (string, int)>();
+    private Dictionary<string, PriorityQueue<string, (int, string)>> Cuisines = new Dictionary<string, PriorityQueue<string, (int, string)>>();
+
+    public FoodRatings(string[] foods, string[] cuisines, int[] ratings)
+    {
+        for (int i = 0; i < foods.Length; i++)
+        {
+            Foods[foods[i]] = (cuisines[i], ratings[i]);
+            if (!Cuisines.ContainsKey(cuisines[i]))
+            {
+                Cuisines[cuisines[i]] = new PriorityQueue<string, (int, string)>();
+            }
+            Cuisines[cuisines[i]].Enqueue(foods[i], (-ratings[i], foods[i]));
+        }
+    }
+
+    public void ChangeRating(string food, int newRating)
+    {
+        Cuisines[Foods[food].Item1].Remove(food, out _, out _);
+        Foods[food] = (Foods[food].Item1, newRating);
+        Cuisines[Foods[food].Item1].Enqueue(food, (-newRating, food));
+    }
+
+    public string HighestRated(string cuisine)
+    {
+        return Cuisines[cuisine].Peek();
+    }
+}
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings obj = new FoodRatings(foods, cuisines, ratings);
+ * obj.ChangeRating(food,newRating);
+ * string param_2 = obj.HighestRated(cuisine);
+ */
