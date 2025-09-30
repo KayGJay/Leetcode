27b06dd386ce5b09e81ebca3695ca4d36dbb7548
Kyle Jones <kylegjones13@gmail.com>
2025-09-30 14:51:39 -0500

    Added bit manip solution. Super cool!

--- 89_gray_code_med/Program.cs
+++ 89_gray_code_med/Program.cs
@@ -1,29 +1,41 @@
-﻿foreach (int num in new Solution().GrayCode(2)) Console.WriteLine(num);
+﻿foreach (int num in new Solution().GrayCode(4)) Console.WriteLine(num);
 
+Console.WriteLine(1 >> 1);
+Console.WriteLine(1 << 1);
 public class Solution
 {
+    //public IList<int> GrayCode(int n)
+    //{
+    //    if (n == 1)
+    //    {
+    //        return new List<int>() { 0, 1 };
+    //    }
+    //    else
+    //    {
+    //        List<int> result = new List<int>();
+    //        List<int> prev = GrayCode(n - 1).ToList();
+    //        foreach (int num in prev)
+    //        {
+    //            Console.WriteLine(num);
+    //            result.Add(num);
+    //        }
+    //        prev.Reverse();
+    //        foreach (int num in prev)
+    //        {
+    //            Console.WriteLine(num);
+    //            result.Add(num + (int)Math.Pow(2, n - 1));
+    //        }
+    //        return result;
+    //    }
+    //}
     public IList<int> GrayCode(int n)
     {
-        if (n == 1)
+        int size = 1 << n;
+        List<int> result = new List<int>();
+        for (int i = 0; i < size; i++)
         {
-            return new List<int>() { 0, 1 };
+            result.Add(i ^ (i >> 1));
         }
-        else
-        {
-            List<int> result = new List<int>();
-            List<int> prev = GrayCode(n - 1).ToList();
-            foreach (int num in prev)
-            {
-                Console.WriteLine(num);
-                result.Add(num);
-            }
-            prev.Reverse();
-            foreach (int num in prev)
-            {
-                Console.WriteLine(num);
-                result.Add(num + (int)Math.Pow(2, n - 1));
-            }
-            return result;
-        }
+        return result;
     }
 }
