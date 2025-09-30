6b86384850d0ebe57c54fb988aef66844c490a5f
Kyle Jones <kylegjones13@gmail.com>
2025-09-30 14:46:40 -0500

    Solved, going to add bit manipulation linear time solution

---
+++ 89_gray_code_med/Program.cs
@@ -0,0 +1,29 @@
+﻿foreach (int num in new Solution().GrayCode(2)) Console.WriteLine(num);
+
+public class Solution
+{
+    public IList<int> GrayCode(int n)
+    {
+        if (n == 1)
+        {
+            return new List<int>() { 0, 1 };
+        }
+        else
+        {
+            List<int> result = new List<int>();
+            List<int> prev = GrayCode(n - 1).ToList();
+            foreach (int num in prev)
+            {
+                Console.WriteLine(num);
+                result.Add(num);
+            }
+            prev.Reverse();
+            foreach (int num in prev)
+            {
+                Console.WriteLine(num);
+                result.Add(num + (int)Math.Pow(2, n - 1));
+            }
+            return result;
+        }
+    }
+}
